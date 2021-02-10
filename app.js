28aa5b9fb274b02852fbd8db2ded883b9cdeb9af
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-02-10 15:06:28 -0700

    scoreboard

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let playerItem = "Nothing"
 let cpuItem = "Nothing"
 let result = "haven't started"
+var playerWins = 0;
+var cpuWins = 0;
 
 
 function announce(){
     let winner = document.getElementById("who-won")
     winner.innerText = `You chose ${playerItem}, the CPU chose ${cpuItem}, you ${result}`
+    let playWins = document.getElementById("player-wins")
+    playWins.innerText = `You have ${playerWins} wins`;
+    let computerWins = document.getElementById("computer-wins")
+    computerWins.innerText = `The CPU has ${cpuWins} wins`
 }
 
 function ifRock(){
@@ -15,6 +21,7 @@ function ifRock(){
         playerItem = "Rock"
         cpuItem = "Paper"
         result = "lose"
+        cpuWins = cpuWins +1
         console.log("You chose Rock, the CPU chose Paper, you lose");
     }else if(RPS==1){
         playerItem = "Rock"
@@ -25,6 +32,7 @@ function ifRock(){
         playerItem = "Rock"
         cpuItem = "Scissors"
         result = "win"
+        playerWins = playerWins + 1
         console.log("You chose Rock, the CPU chose Scissors, you win");}
     announce()
     }
@@ -34,6 +42,7 @@ function ifPaper(){
         playerItem = "Paper"
         cpuItem = "Scissors"
         result = "lose"
+        cpuWins = cpuWins +1
         console.log("You chose Paper, the CPU chose Scissors, you lose");
     }else if(RPS==1){
         playerItem = "Paper"
@@ -44,6 +53,7 @@ function ifPaper(){
         playerItem = "Paper"
         cpuItem = "Rock"
         result = "win"
+        playerWins = playerWins + 1
         console.log("You chose Paper, the CPU chose Rock, you win");}
     announce()
     }
@@ -53,6 +63,7 @@ function ifScissors(){
         playerItem = "Scissors"
         cpuItem = "Rock"
         result = "lose"
+        cpuWins = cpuWins +1
         console.log("You chose Scissors, the CPU chose Rock, you lose");
     }else if(RPS==1){
         playerItem = "Scissors"
@@ -63,6 +74,7 @@ function ifScissors(){
         playerItem = "Scissors"
         cpuItem = "Paper"
         result = "win"
+        playerWins = playerWins + 1
         console.log("You chose Scissors, the CPU chose Paper, you win");}
     announce()
     }
